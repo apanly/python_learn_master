9e8d09826147bd74ab0027de763b5fa43083f071
vincent <apanly@163.com>
2020-12-08 22:54:13 +0800

    guowei  — demo 增加域名配置

--- config/local_setting_demo.py
+++ config/local_setting_demo.py
@@ -10,3 +10,10 @@ SQLALCHEMY_TRACK_MODIFICATIONS=False
 SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/learn_master?charset=utf8mb4'
 SQLALCHEMY_ENCODING = "utf8mb4"
 
+## 域名配置
+DOMAIN = {
+    "www": "http://127.0.0.1:" + str( SERVER_PORT ),
+    "api": "http://127.0.0.1:" + str( API_SERVER_PORT ),
+    "resource" : "http://127.0.0.1:" + str( SERVER_PORT ) + "/static/cdn"
+}
+
