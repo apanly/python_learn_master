55486a30b9c8af99981b2e3ba10e1db9ffe7ba77
vincent <apanly@163.com>
2021-01-19 21:55:15 +0800

    guowei  — 公共的方法

--- common/components/helper/ModelHelper.py
+++ common/components/helper/ModelHelper.py
@@ -57,3 +57,14 @@ class ModelHelper(BaseService):
                 break
             ret.append( getattr(item, key_field) )
         return ret
+
+    '''
+    将model对象转成dict对象
+    '''
+    @staticmethod
+    def model2Dict(model):
+        if model is None:
+            return dict()
+        from sqlalchemy.orm import class_mapper
+        columns = [c.key for c in class_mapper(model.__class__).columns]
+        return dict((c, getattr(model, c)) for c in columns)
--- jobs/BaseJob.py
+++ jobs/BaseJob.py
@@ -4,4 +4,8 @@
 基础Job类
 '''
 class BaseJob():
-    pass
+    def exitOK(self):
+        return 0
+
+    def exitFail(self,exit_code = 1):
+        return exit_code
